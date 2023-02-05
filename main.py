b2379702615e828850a552cb85803b12dc992561
Kushagra Mehta <Kushagra8@users.noreply.github.com>
2023-02-05 15:58:42 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,96 @@
+# Python program for implementation of various sorting algorithms
+
+def selection_sort(arr):
+    """
+    Selection sort algorithm implementation
+    """
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+def insertion_sort(arr):
+    """
+    Insertion sort algorithm implementation
+    """
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+def bubble_sort(arr):
+    """
+    Bubble sort algorithm implementation
+    """
+    for i in range(len(arr)):
+        for j in range(0, len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+def merge_sort(arr):
+    """
+    Merge sort algorithm implementation
+    """
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        merge_sort(L)
+        merge_sort(R)
+
+        i = j = k = 0
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+    return arr
+
+def partition(arr, low, high):
+    """
+    Helper function for quick sort algorithm
+    """
+    i = low - 1
+    pivot = arr[high]
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+def quick_sort(arr, low, high):
+    """
+    Quick sort algorithm implementation
+    """
+    if low < high:
+        pi = partition(arr, low, high)
+        quick_sort(arr, low, pi - 1)
+        quick_sort(arr, pi + 1, high)
+    return arr
+
+# test the sorting algorithms
+arr = [64, 34, 25, 12, 22, 11, 90]
+print("Selection Sort: ", selection_sort(arr))
+print("Bubble Sort: ", bubble_sort(arr))
+print("Insertion Sort: ", insertion_sort(arr))
